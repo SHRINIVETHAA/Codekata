7a5d8954c601a805183869f790e0cd90614dbfa0
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2018-01-09 11:42:10 +0530

    Update vowel or consonant.c

--- Beginner/Set-1/vowel or consonant.c
+++ Beginner/Set-1/vowel or consonant.c
@@ -6,11 +6,11 @@ int main()
     scanf("%c",&x);
     if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
     { 
-        printf("%c is a vowel",x);
+        printf("vowel");
     }
     else
     {
-        printf("%c is an consonant",x);
+        printf("consonant");
     }
     return 0;
 }    

11a8bd7df038dff62ca69015dfaca641bbf149a0
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2018-01-09 11:33:10 +0530

    Create odd or even.c

---
+++ Beginner/Set-1/odd or even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any number:\n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("%d is even",n);
+    }
+    else
+    {
+        printf("%d is odd");
+    }
+    return 0;
+}

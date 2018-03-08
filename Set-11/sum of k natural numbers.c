e7d0f3ce5a21165f04e605cfcc7949ad4650dcd9
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2018-03-08 09:28:13 +0530

    Update sum of k natural numbers.c

--- Set-11/sum of k natural numbers.c
+++ Set-11/sum of k natural numbers.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,sum=0,i;
-    scanf("%d",&n);
-    for(i=0;i<=n;i++)
+    int k,sum=0,i;
+    scanf("%d",&k);
+    for(i=0;i<=k;i++)
     {
         sum=sum+i;
     }

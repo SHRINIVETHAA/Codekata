ece1e65953d7b38ae271dd4ce080455299ca8606
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2018-06-01 22:51:24 +0530

    Create print first k characters of a string.c

---
+++ Set-7/print first k characters of a string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[10];
+    int i,length,k;
+    scanf("%s %d",s,&k);
+    for(i=0;i<k;i++)
+    {
+       printf("%c",s[i]) ;
+       
+    }
+    return 0;
+}

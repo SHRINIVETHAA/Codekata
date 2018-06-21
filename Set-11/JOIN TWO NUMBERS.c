79ebc0026612312066e2768f38afda043fecb6d8
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2018-06-21 13:03:20 +0530

    Update JOIN TWO NUMBERS.c

--- Set-11/JOIN TWO NUMBERS.c
+++ Set-11/JOIN TWO NUMBERS.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n1,n2;
-    scanf("%d %d",&n1,&n2);
-    printf("%d%d",n1,n2);
+    int n,k;
+    scanf("%d %d",&n,&k);
+    printf("%d%d",n,k);
     return 0;   
 }
